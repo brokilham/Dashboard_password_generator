d49ea6c872834a4e16030bd08eb7470ceeb92d9f
brokilham <plato.ilham@gmail.com>
2018-11-07 22:44:44 +0700

    first init v.1

---
+++ Dashboard_password_generator/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Dashboard_password_generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.btn_encrypt = new System.Windows.Forms.Button();
+            this.txt_key = new System.Windows.Forms.TextBox();
+            this.txt_result_pass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(63, 19);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(184, 20);
+            this.txt_password.TabIndex = 0;
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Location = new System.Drawing.Point(282, 71);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_cancel.TabIndex = 2;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // btn_encrypt
+            // 
+            this.btn_encrypt.Location = new System.Drawing.Point(282, 19);
+            this.btn_encrypt.Name = "btn_encrypt";
+            this.btn_encrypt.Size = new System.Drawing.Size(75, 23);
+            this.btn_encrypt.TabIndex = 3;
+            this.btn_encrypt.Text = " Encrypt";
+            this.btn_encrypt.UseVisualStyleBackColor = true;
+            this.btn_encrypt.Click += new System.EventHandler(this.btn_encrypt_Click);
+            // 
+            // txt_key
+            // 
+            this.txt_key.Location = new System.Drawing.Point(63, 45);
+            this.txt_key.Name = "txt_key";
+            this.txt_key.Size = new System.Drawing.Size(184, 20);
+            this.txt_key.TabIndex = 4;
+            // 
+            // txt_result_pass
+            // 
+            this.txt_result_pass.Location = new System.Drawing.Point(63, 71);
+            this.txt_result_pass.Name = "txt_result_pass";
+            this.txt_result_pass.ReadOnly = true;
+            this.txt_result_pass.Size = new System.Drawing.Size(184, 20);
+            this.txt_result_pass.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Result";
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(282, 45);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_clear.TabIndex = 9;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 111);
+            this.ControlBox = false;
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_result_pass);
+            this.Controls.Add(this.txt_key);
+            this.Controls.Add(this.btn_encrypt);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.txt_password);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.Button btn_encrypt;
+        private System.Windows.Forms.TextBox txt_key;
+        private System.Windows.Forms.TextBox txt_result_pass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_clear;
+    }
+}
+
---
+++ Dashboard_password_generator/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace Dashboard_password_generator
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_encrypt_Click(object sender, EventArgs e)
+        {
+            
+            string password =  txt_password.Text;
+            string key = txt_key.Text;
+
+            if (password.Length > 0 && key.Length > 0) {
+                txt_result_pass.Text =  MD5_encryptor(key, password);
+            }
+        }
+
+        private string MD5_encryptor(string key, string password) {
+            
+            var provider = MD5.Create();
+
+            byte[] bytes = provider.ComputeHash(Encoding.ASCII.GetBytes(key + password + key));
+            string computedHash = BitConverter.ToString(bytes);
+            return computedHash.Replace("-", "");
+        }
+
+ 
+
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            txt_password.Text = "";
+            txt_key.Text = "";
+            txt_result_pass.Text = "";
+        }
+    }
+}
